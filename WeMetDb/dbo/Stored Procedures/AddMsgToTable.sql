3c743a2d9976b203d1deba9f65f89aeb401fadb6
madhukar-dotnet <55914338+madhukar-dotnet@users.noreply.github.com>
2023-02-08 07:58:21 +0530

    db added

---
+++ WeMetDb/dbo/Stored Procedures/AddMsgToTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE procedure [dbo].[AddMsgToTable]
+@msg nvarchar(MAX),
+@SenderName nvarchar(50),
+@ReciverName nvarchar(50)
+as
+begin
+	Insert into msgtable (chatMsg, MsgSender, msgReceiver) values(@msg, @SenderName, @ReciverName)
+end
